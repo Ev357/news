d9f8c3349d1654015be44ae5e835b6047bf15f68
Ev357 <evest.dev@gmail.com>
2024-04-30 12:22:34 +0200

    feat: some post display

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import { usePostStore } from '@/stores/post';
 import { storeToRefs } from 'pinia';
 import { onMounted } from 'vue';
 
-const { isPostOpen } = storeToRefs(usePostStore());
+const { isPostOpen, post } = storeToRefs(usePostStore());
 const { getPosts } = usePostStore();
 
 onMounted(() => {
@@ -19,7 +19,7 @@ onMounted(() => {
     <Header />
     <main class="flex justify-center px-2 pb-2 pt-6">
       <Posts v-if="!isPostOpen" />
-      <Post v-else />
+      <Post v-else-if="post" :post />
     </main>
   </div>
 </template>
--- src/components/Post.vue
+++ src/components/Post.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import Breadcrumb from '@/components/Breadcrumb.vue';
-import { usePostStore } from '@/stores/post';
-import { storeToRefs } from 'pinia';
+import type { Post } from '@/composables/useGetPosts';
 
-const { post } = storeToRefs(usePostStore());
+defineProps<{
+  post: Post;
+}>();
 </script>
 
 <template>
-  <div>
-    <Breadcrumb />
-    <div>{{ post }}</div>
+  <div class="flex w-full flex-col gap-2 md:max-w-screen-md">
+    <Breadcrumb class="pb-10" />
+    <h1 class="text-balance text-center text-4xl">{{ post.title }}</h1>
   </div>
 </template>
